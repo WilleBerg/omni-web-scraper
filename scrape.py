21d38fdc24d3caf29187582dd86890228083b77c
jam <william.berg2@gmail.com>
2025-03-07 20:53:54 +0000

    Changed port.

--- scrape.py
+++ scrape.py
@@ -84,4 +84,4 @@ def save_to_json(articles, filename='news_articles.json'):
         print(f"Failed to save articles to JSON: {e}")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5001)
